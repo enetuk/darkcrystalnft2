b3d7537739e77b0fcdbe9176c6188ac45031a74e
enetuk <maksimduhanov@Maksims-MacBook-Air.local>
2022-05-01 17:43:33 +0300

    install change

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 yarn install
-git clone https://github.com/metaplex-foundation/metaplex.git
+cd solana_integration && git clone https://github.com/metaplex-foundation/metaplex.git
 cd metaplex/js && yarn install && yarn bootstrap && yarn build
 cd ~/darkcrystalnft2 && sudo ./setup_domain.sh

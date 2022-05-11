4fac8518a27e07fea0a3ae4c855727e32dec7382
enetuk <maksimduhanov@Maksims-MacBook-Air.local>
2022-05-11 20:53:27 +0300

    fugible assets

--- solana_integration/fungible_assets.js
+++ solana_integration/fungible_assets.js
@@ -1,6 +1,8 @@
 const mpl_token_metadata_1 = require("../solana_integration/metaplex/js/node_modules/@metaplex-foundation/mpl-token-metadata");
 const anchor = require("../solana_integration/metaplex/js/node_modules/@project-serum/anchor");
 
+const spl_token_1 = require("../solana_integration/metaplex/js/node_modules/@solana/spl-token");
+
 const mint_nft = require("../solana_integration/metaplex/js/packages/cli/build/commands/mint-nft");
 
 
